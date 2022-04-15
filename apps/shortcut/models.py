891cbb94fe7de97de92d015d31a3ec73ea74bced
西格玛（吴超凡） <wuchaofan@soyoung.com>
2022-04-15 21:45:57 +0800

    update

--- apps/shortcut/models.py
+++ apps/shortcut/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+from frames.models import BaseModel
+
+
+class Shortcut(BaseModel):
+    name = models.CharField(default='', max_length=100)
+    desc = models.IntegerField(default=1)
+    dept = models.IntegerField(default=1)
+    module = models.IntegerField(default=1)
+    cmd = models.CharField(default='', max_length=100)
+    exe_cnt = models.TextField(default='')
+    curr_page = models.CharField(default='', max_length=100)
+    curr_page_ext = models.TextField(default='')
+    from_action = models.CharField(default='', max_length=100)
+    from_action_ext = models.TextField(default='')
+
+    class Meta:
+        verbose_name = '标准埋点表'
+        verbose_name_plural = verbose_name
+
+
+class PointExtend(BaseModel):
+    ctime = models.IntegerField(default=0)
+    point_type = models.IntegerField(default=1)
+    from_page = models.CharField(default='', max_length=100)
+    from_page_ext = models.TextField(default='')
+    curr_page = models.CharField(default='', max_length=100)
+    curr_page_ext = models.TextField(default='')
+    from_action = models.CharField(default='', max_length=100)
+    from_action_ext = models.TextField(default='')
+    details = models.TextField(default='')
+
+    class Meta:
+        verbose_name = '拦截埋点记录表'
+        verbose_name_plural = verbose_name
+
+
+
+
--- web/vue.config.js
+++ web/vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || process.env.VUE_APP_TITLE || 'Tester管理系统' // 标题
+const name = defaultSettings.title || process.env.VUE_APP_TITLE || '测试管理系统' // 标题
 const port = process.env.port || process.env.npm_config_port || 80 // 端口
 
 // vue.config.js 配置说明
